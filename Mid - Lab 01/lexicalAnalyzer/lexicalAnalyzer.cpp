796be0ceb11b82279e0e35ededc0fef104f011ab
Iftekhar Uddin Mullick <infomullick2000@gmail.com>
2023-10-03 10:56:22 +0600

    Update 2
    Bug fix
    > Fixed a bug where single digit numerics would print misrepresented output.
    > Commented the code.
    
    Future requirements:
    > Big numerics can be used.

--- Mid - Lab 01/lexicalAnalyzer/lexicalAnalyzer.cpp
+++ Mid - Lab 01/lexicalAnalyzer/lexicalAnalyzer.cpp
@@ -1,44 +1,68 @@
 #include<iostream>
 #include<cctype>
+#include<string>
 
 using namespace std;
 
 int main(){
     string statement; //To Store The String Data for Compiler Use
-    int numberOfCharInStatement = 1;
-    int syntaxTableManagerIndex = 0;
+    int numberOfCharInStatement = 1; //To store size of statement
+    int syntaxTableManagerIndex = 0; //Suntax table index manager
+    //string temporaryNumericCharacterStorage;
     
+    // Taking input of statement
     cout << "Provide input: " ;
-    getline(cin, statement);
+    getline(cin, statement); //Requires cctype library
     cout << endl;
     
+    // Print statement provided
     cout << "Input String => " << statement << endl;
     
-    // counting the number of characters
+    // Counting the number of characters by using foreach
     for(char i : statement){
         numberOfCharInStatement++;
+        // Checking the end of a statement
         if(i == ';'){
             break;
         }
     }
     
     cout << endl;
-
+    
+    // Result section
+    cout << "Lexical Result => " ;
+    
+    // Checking characters one by one
     for(int i=0; i < numberOfCharInStatement; i++){
+        // Check if the character is Alphabatic of not
         if (isalpha(statement[i])){
+            // Checking if the following character is Alphabatic or not. Else print Token
             if (!isalpha(statement[i+1])){
                 cout << "<id," << syntaxTableManagerIndex++ << ">";
             }
         }
         
+        // Check if the the following character is Numeric or not
         else if (isdigit(statement[i])){
+            string temporaryNumericCharacterStorage;
+            // Checking if the following character is Numeric or not. Else print Token
             if (!isdigit(statement[i+1])){
-                cout << "<" << statement[i-1] << statement[i] << ">";
+                cout << "<" << temporaryNumericCharacterStorage << ">";
+                /*
+                if (statement[i-1] == '+' || statement[i-1] == '-' || statement[i-1] == '*' || statement[i-1] == '/' || statement[i-1] == '%' || statement[i-1] == '='){
+                    cout << "<" << statement[i] << ">";
+                }
+                else{
+                    cout << "<" << statement[i-1] << statement[i] << ">";
+                }
+                */
             }
         }
         
+        // Check if the following is an operator or not
         else if (statement[i] == '+' || statement[i] == '-' || statement[i] == '*' || statement[i] == '/' || statement[i] == '%' || statement[i] == '=' || statement[i] == ';'){
             cout << "<" << statement[i] << ">";
+            // Checking the end of a statement
             if(statement[i] == ';'){
                 break;
             }
